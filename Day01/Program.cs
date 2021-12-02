ac74a146efc2e9b3732d7aa4600e8463fd57d1b4
David <info@vidmar.net>
2021-12-02 07:00:46 +0100

    Day 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Linq;
+﻿using System.Linq;
 using static AdventOfCode2021.Utils;
 
 class Program
---
+++ Day02/Program.cs
@@ -0,0 +1,66 @@
+﻿using static AdventOfCode2021.Utils;
+
+int result1 = 0;
+int result2 = 0;
+
+StartDay(2);
+
+// Part 1
+StartPart(1);
+
+var pos = 0;
+var depth = 0;
+
+var lines = ReadInputAsLines();
+
+foreach (var line in lines)
+{
+    var s = line.Split(' ');
+
+    var way = s[0];
+    var amount = Convert.ToInt32(s[1]);
+
+    if (way == "forward")
+        pos += amount;
+    if (way == "up")
+        depth -= amount;
+    if (way == "down")
+        depth += amount;
+}
+
+LogLine($"pos = {pos}, depth = {depth}");
+result1 = pos * depth;
+
+EndPart(1, result1);
+
+// Part 2
+StartPart(2);
+
+var aim = 0;
+pos = 0;
+depth = 0;
+
+foreach (var line in lines)
+{
+    var s = line.Split(' ');
+
+    var way = s[0];
+    var amount = Convert.ToInt32(s[1]);
+
+    if (way == "forward")
+    {
+        pos += amount;
+        depth += aim * amount;
+    }
+    if (way == "up")
+        aim -= amount;
+    if (way == "down")
+        aim += amount;
+
+    LogLine($"pos = {pos}, depth = {depth}, aim = {aim}");
+}
+
+LogLine($"pos = {pos}, depth = {depth}");
+result2 = pos * depth;
+
+EndPart(2, result2);
